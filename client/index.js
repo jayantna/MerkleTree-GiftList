fb1d0a7d62f6c753df0494a63e6f697032581989
jayantna <jonty.nagle@gmail.com>
2023-05-17 22:45:14 +0530

    final

--- client/index.js
+++ client/index.js
@@ -5,10 +5,21 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
+  const arg = process.argv
+  if(arg.length<3){
+    console.log('Enter name in command line argument')
+    return
+  }
+  const name = arg.splice(2).join(' ')
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: name,
+    proof: proof
   });
 
   console.log({ gift });
---
+++ utils/getRoot.js
@@ -0,0 +1,4 @@
+const niceList = require('./niceList.json');
+const MerkleTree = require('./MerkleTree');
+
+console.log(MerkleTree.getRoot());
